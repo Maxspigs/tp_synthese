dae28179eeb0788c22b6d746de8747204682828e
Unknown <jay.vrm.faim@gmail.com>
2018-05-24 15:20:28 -0400

    désolé

--- include/noeudLex.hpp
+++ include/noeudLex.hpp
@@ -3,12 +3,17 @@
 using namespace std;
 struct NoeudLex {
   char lettre;
+  NoeudLex* parent;
   map<char, NoeudLex*> enfants;
-  NoeudLex(string val = ''){
+  NoeudLex(string val = '',NoeudLex *n = NULL){
   	lettre = val;
+  	parent = n;
   	enfants = NULL;
   };
   void addChild(NoeudLex *n){
   	enfants.insert({n->lettre,n});
   };
+  void afficheNoeud(){
+
+  }
 };
